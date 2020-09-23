990b544c452fb0ddfe6c39938a2358a8c988577f
Haşim Yerlikaya <yerlikayahasim@gmail.com>
2020-09-23 23:12:24 +0300

    Background and splash color are made optional.

--- lib/salomon_bottom_bar.dart
+++ lib/salomon_bottom_bar.dart
@@ -15,6 +15,8 @@ class SalomonBottomBar extends StatelessWidget {
     this.itemPadding = const EdgeInsets.symmetric(vertical: 10, horizontal: 16),
     this.duration = const Duration(milliseconds: 500),
     this.curve = Curves.easeOutQuint,
+    this.showBackroundColor = true,
+    this.showSplashColor = true,
   }) : super(key: key);
 
   /// A list of tabs to display, ie `Home`, `Likes`, etc
@@ -44,6 +46,12 @@ class SalomonBottomBar extends StatelessWidget {
   /// The transition curve
   final Curve curve;
 
+  /// The selected item background color
+  final bool showBackroundColor;
+
+  /// The ripple effect when any item tapped
+  final bool showSplashColor;
+
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
@@ -61,34 +69,27 @@ class SalomonBottomBar extends StatelessWidget {
               curve: curve,
               duration: duration,
               builder: (context, t, _) {
-                final _selectedColor = item.selectedColor ??
-                    selectedItemColor ??
-                    theme.primaryColor;
+                final _selectedColor = item.selectedColor ?? selectedItemColor ?? theme.primaryColor;
 
-                final _unselectedColor = item.unselectedColor ??
-                    unselectedItemColor ??
-                    theme.iconTheme.color;
+                final _unselectedColor = item.unselectedColor ?? unselectedItemColor ?? theme.iconTheme.color;
 
                 return Material(
-                  color: Color.lerp(_selectedColor.withOpacity(0.0),
-                      _selectedColor.withOpacity(0.1), t),
+                  color: showBackroundColor ? Color.lerp(_selectedColor.withOpacity(0.0), _selectedColor.withOpacity(0.1), t) : Colors.transparent,
                   shape: StadiumBorder(),
                   child: InkWell(
                     onTap: () => onTap?.call(items.indexOf(item)),
                     customBorder: StadiumBorder(),
-                    focusColor: _selectedColor.withOpacity(0.1),
-                    highlightColor: _selectedColor.withOpacity(0.1),
-                    splashColor: _selectedColor.withOpacity(0.1),
+                    focusColor: showSplashColor ? _selectedColor.withOpacity(0.1) : Colors.transparent,
+                    highlightColor: showSplashColor ? _selectedColor.withOpacity(0.1) : Colors.transparent,
+                    splashColor: showSplashColor ? _selectedColor.withOpacity(0.1) : Colors.transparent,
                     hoverColor: _selectedColor.withOpacity(0.1),
                     child: Padding(
-                      padding: itemPadding -
-                          EdgeInsets.only(right: itemPadding.right * t),
+                      padding: itemPadding - EdgeInsets.only(right: itemPadding.right * t),
                       child: Row(
                         children: [
                           IconTheme(
                             data: IconThemeData(
-                              color: Color.lerp(
-                                  _unselectedColor, _selectedColor, t),
+                              color: Color.lerp(_unselectedColor, _selectedColor, t),
                               size: 24,
                             ),
                             child: item.icon ?? SizedBox.shrink(),
@@ -104,15 +105,10 @@ class SalomonBottomBar extends StatelessWidget {
                                 alignment: Alignment(-0.2, 0.0),
                                 widthFactor: t,
                                 child: Padding(
-                                  padding: EdgeInsets.only(
-                                      left: itemPadding.right / 2,
-                                      right: itemPadding.right),
+                                  padding: EdgeInsets.only(left: itemPadding.right / 2, right: itemPadding.right),
                                   child: DefaultTextStyle(
                                     style: TextStyle(
-                                      color: Color.lerp(
-                                          _selectedColor.withOpacity(0.0),
-                                          _selectedColor,
-                                          t),
+                                      color: Color.lerp(_selectedColor.withOpacity(0.0), _selectedColor, t),
                                       fontWeight: FontWeight.w600,
                                     ),
                                     child: item.title ?? SizedBox.shrink(),
